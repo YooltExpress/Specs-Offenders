e0508e1d3552447e40d6f0a489d31f9331cb746b
Htoops <a02392957@usu.edu>
2025-06-04 13:25:30 -0600

    Checking to see if it's our code that fails.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Map from './Map';
+
 
 const App: React.FC = () => {
   const initialCenter: [number, number] = [40.7608, -111.8910];
@@ -7,9 +7,7 @@ const App: React.FC = () => {
 
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>My OpenStreetMap Application</h1>
-      <p>Displaying Salt Lake City using OpenStreetMap data.</p>
-      <Map center={initialCenter} zoom={initialZoom} />
+      Hello World.
     </div>
   );
 };
