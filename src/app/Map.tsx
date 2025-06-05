4b58922a07b8a1473ea17741a35c2fe45db250db
Htoops <a02392957@usu.edu>
2025-06-05 09:52:31 -0600

    Added a feature that lets you drop a pin anywhere on the map.

--- src/app/Map.tsx
+++ src/app/Map.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
@@ -23,6 +23,23 @@ interface MapProps {
 const Map: React.FC<MapProps> = ({ center, zoom }) => {
   // Use a state to generate a unique key for the MapContainer
   const [mapKey, setMapKey] = useState(Date.now());
+  const [pinPosition, setPinPosition] = useState<[number, number] | null>(null);
+function DropPin() {
+  useMapEvents({
+    click(e) {
+      setPinPosition([e.latlng.lat, e.latlng.lng]);
+    },
+  });
+
+  return pinPosition ? (
+    <Marker position={pinPosition}>
+      <Popup>
+        Dropped Pin<br />
+        Lat: {pinPosition[0].toFixed(4)}, Lng: {pinPosition[1].toFixed(4)}
+      </Popup>
+    </Marker>
+  ) : null;
+  }
   
   // Clean up the map instance when component unmounts
   useEffect(() => {
@@ -53,10 +70,11 @@ const Map: React.FC<MapProps> = ({ center, zoom }) => {
       />
       {/* Example Marker */}
       <Marker position={center}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+    </Marker>
+    <DropPin />
     </MapContainer>
   );
 };
